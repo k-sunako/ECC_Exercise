550eca3accd19b47481f50afdb62f1229ec4c18c
k-sunako <dd.kazumasa@gmail.com>
2025-08-13 15:10:03 +0900

    問題15.20のシンドロームの計算を実装

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """."""
 
 import numpy as np
-
+import itertools
 
 I_hat = np.eye(4, dtype=np.int32)
 J = np.full((4, 4), 1, dtype=np.int32)
@@ -14,5 +14,12 @@ A = np.block([
 
 G = np.block([np.eye(16, dtype=np.int32), A])
 
-H = np.block([-A.T, np.eye(16)])
-print(H)
+# H = np.block([-A.T, np.eye(16)])
+H = np.block([A.T, np.eye(16)])
+
+
+for tp_ind in itertools.combinations(range(32), 2):
+    e = np.zeros(32)
+    for i in tp_ind:
+        e[i] = 1
+    s = H @ e.T
